ef94ce013dc52cd6c7a2856a5f3b825c9f65f129
Brian Connolly <Brian.connolly71@gmail.com>
2021-04-10 12:59:49 +0100

    added basic js functionality

---
+++ script.js
@@ -0,0 +1,9 @@
+const form = document.querySelector(".top-banner form");
+
+form.addEventListener("submit", e =>{
+    e.preventDefault();//prevents reload of page
+    const inputVal = input.value;//gets the value the user entered into the searchbar
+})
+
+const apiKey = "YO73d94735253c37eef5165eb09f430c33Y";
+const inputVal = input.value;
